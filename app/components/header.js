0fd3316d474e9110ec91997f745aa91803cc2483
Payton Swick <payton@foolord.com>
2016-01-30 23:14:23 -0500

    Undo navbar and make header navbar-like

--- app/components/header.js
+++ app/components/header.js
@@ -43,14 +43,12 @@ export default React.createClass( {
 
   render() {
     return (
-      <div className="header navbar navbar-default">
-        <div className="container">
-          <h1 className="header__title">Voyageur</h1>
-          <img className="header__logo navbar-brand" src="/assets/logo.png" />
-          <div className="header__buttons">
-            { this.renderAdminButton() }
-            <button className="btn btn-default log-out-button navbar-btn" onClick={ this.props.onLogOut }>Log out</button>
-          </div>
+      <div className="header">
+        <h1 className="header__title">Voyageur</h1>
+        <img className="header__logo" src="/assets/logo.png" />
+        <div className="header__buttons">
+          { this.renderAdminButton() }
+          <button className="btn btn-default log-out-button" onClick={ this.props.onLogOut }>Log out</button>
         </div>
         { this.renderNotices() }
       </div>
