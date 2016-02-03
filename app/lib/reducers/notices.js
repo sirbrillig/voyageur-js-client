251a08eb1d691f9e5cad498e8477081f2d23c7a6
Payton Swick <payton@foolord.com>
2016-02-03 17:21:53 -0500

    Improve error messages for distance failures

--- app/lib/reducers/notices.js
+++ app/lib/reducers/notices.js
@@ -2,7 +2,10 @@ const initialState = { errors: [] };
 export default function auth( state = initialState, action ) {
   switch ( action.type ) {
     case 'ERROR':
-      const text = action.error.toString();
+      let text = action.error.response ? action.error.response.text : action.error.toString();
+      if ( text.match( /ZERO/i ) ) {
+        text += '; Maybe check that your addresses are valid?';
+      }
       const errors = [ ...state.errors, text ];
       return Object.assign( {}, state, { errors } );
     case 'NOTICES_CLEAR':
