a3338d604e454cf8ba74761f9052a0d15ae4757a
Payton Swick <payton@foolord.com>
2016-02-10 18:58:09 -0500

    Use browserHistory instead of history

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Router, Route, IndexRoute } from 'react-router';
-import createBrowserHistory from 'history/lib/createBrowserHistory'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './lib/store';
 import App from './components/app';
@@ -8,11 +7,9 @@ import AdminDashboard from './components/admin-dashboard';
 import ImportPanel from './components/import';
 import LoggedIn from './components/logged-in';
 
-const history = createBrowserHistory();
-
 export default (
   <Provider store={ store }>
-    <Router history={ history }>
+    <Router history={ browserHistory }>
       <Route path="/" component={ App }>
         <IndexRoute component={ LoggedIn } />
         <Route path="/admin" component={ AdminDashboard } />
