db4cdc82eaeed608a6f01bb3b2972fa737aeb423
Payton Swick <payton@foolord.com>
2016-02-12 20:31:14 -0500

    Style log-in-box to match loading-panel

--- app/components/log-in-box.js
+++ app/components/log-in-box.js
@@ -7,11 +7,13 @@ export default React.createClass( {
 
   render: function() {
     return (
-      <div className="log-in-box auth0-box before">
-        <img className="log-in-box__logo" src="/assets/logo.png" />
-        <h1 className="log-in-box__title">Voyageur</h1>
-        <p className="log-in-box__subtitle">How far do you go?</p>
-        <a onClick={ this.props.showAuth } className="log-in-box__button btn btn-primary btn-lg btn-block">Let's Go!</a>
+      <div className="log-in-box">
+        <div className="log-in-box__content">
+          <img className="log-in-box__logo animated flipInX" src="/assets/logo-medium-smooth.png" />
+          <h1 className="log-in-box__title">Voyageur</h1>
+          <p className="log-in-box__subtitle">How far do you go?</p>
+          <a onClick={ this.props.showAuth } className="log-in-box__button btn btn-primary btn-lg">Let's find out!</a>
+        </div>
       </div>
     );
   }
