c5722d6fd446236a27f4c38247300fc3f10af3bc
Payton Swick <payton@foolord.com>
2016-02-13 12:40:33 -0500

    Add example config file

---
+++ app/auth0-variables-example.js
@@ -0,0 +1,6 @@
+export default {
+  AUTH0_CLIENT_ID: 'yourauth0clientid',
+  AUTH0_DOMAIN: 'yourauth0clientdomain',
+  AUTH0_CALLBACK_URL: location.href,
+  VOYAGEUR_API_SERVER: 'thevoygeurrestapiserver',
+};
