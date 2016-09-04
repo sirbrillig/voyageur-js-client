7fe5116439be24443005bfb348dc1a7bee07db16
Payton Swick <payton@foolord.com>
2016-09-04 19:23:10 -0400

    Persist cached distances in localstorage

--- app/lib/store.js
+++ app/lib/store.js
@@ -10,7 +10,7 @@ const debug = debugFactory( 'voyageur:store' );
 const createStoreWithMiddleware = compose(
   applyMiddleware( thunk ),
   applyMiddleware( Debug( debug ) ),
-  persistState( [ 'auth', 'prefs' ], { key: 'voyageur' } ),
+  persistState( [ 'auth', 'prefs', 'distances' ], { key: 'voyageur' } ),
   ( window && window.devToolsExtension ) ? window.devToolsExtension() : f => f
 )( createStore );
 
