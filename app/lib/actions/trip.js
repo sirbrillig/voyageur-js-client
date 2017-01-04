48bcfbc680f4743a8dde357b9cb2436654cd75f1
Payton Swick <payton@foolord.com>
2017-01-03 20:28:25 -0500

    Remove outdated Trip actions

--- app/lib/actions/trip.js
+++ app/lib/actions/trip.js
@@ -1,39 +1,17 @@
 import { gotError } from 'lib/actions/general';
-import * as api from 'lib/api/trip';
-import { reorderArray, getKeyForAddresses } from 'lib/helpers';
-import { getAddressPairs, getDistanceForKey, isDistanceExpired } from 'lib/selectors';
+import { getDistanceBetween as getApiDistanceBetween } from 'lib/api/trip';
 
 export function removeTripLocation( index ) {
-  return function( dispatch, getState ) {
-    const ids = getState().trip.slice( 0 ); // copy the array
-    ids.splice( index, 1 ); // remove the id
-    dispatch( gotTrip( ids ) );
-    dispatch( updateDistance() );
-  };
+  return { type: 'TRIP_REMOVE_LOCATION', index };
 }
 
 export function addToTrip( location ) {
   return { type: 'TRIP_ADD_LOCATION', location };
 }
 
-export function updateDistance() {
-  return function( dispatch, getState ) {
-    // split trip into pairs
-    const addrPairs = getAddressPairs( getState() );
-    // find cached pairs
-    const cached = addrPairs.filter( pair => getDistanceForKey( getState(), getKeyForAddresses( pair.start, pair.dest ) ) );
-    // find expired cached pairs
-    const expired = cached.filter( pair => isDistanceExpired( getState(), getKeyForAddresses( pair.start, pair.dest ) ) );
-    // find uncached pairs
-    const uncached = addrPairs.filter( pair => ! getDistanceForKey( getState(), getKeyForAddresses( pair.start, pair.dest ) ) );
-    // fetch distance for each uncached pair
-    uncached.concat( expired ).map( pair => dispatch( fetchDistanceBetween( pair.start, pair.dest ) ) );
-  };
-}
-
 export function fetchDistanceBetween( start, dest ) {
   return function( dispatch, getState ) {
-    api.getDistanceBetween( getState().auth.token, start, dest )
+    getApiDistanceBetween( getState().auth.token, start, dest )
     .then( data => dispatch( gotDistanceBetween( start, dest, data ) ) )
     .catch( err => dispatch( gotError( err ) ) );
   };
@@ -53,22 +31,11 @@ export function gotTrip( trip ) {
 }
 
 export function clearTrip() {
-  return function( dispatch ) {
-    dispatch( gotClearedTrip() );
-  };
-}
-
-export function gotClearedTrip() {
   return { type: 'TRIP_CLEAR' };
 }
 
-export function moveTripLocation( tripLocationIndex, targetLocationIndex ) {
-  return function( dispatch, getState ) {
-    const newTrip = reorderArray( getState().trip, tripLocationIndex, targetLocationIndex );
-    if ( ! newTrip ) return dispatch( gotError( 'Could not find tripLocation data to move it' ) );
-    dispatch( gotTrip( newTrip ) );
-    dispatch( updateDistance() );
-  };
+export function moveTripLocation( startIndex, destinationIndex ) {
+  return { type: 'TRIP_MOVE_LOCATION', startIndex, destinationIndex };
 }
 
 export function changeUnits( unit ) {
