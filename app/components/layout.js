88f6be8df2323bfd8a27e9dac8c080de80300353
Payton Swick <payton@foolord.com>
2017-01-04 15:57:12 -0500

    Prevent orphans in footer

--- app/components/layout.js
+++ app/components/layout.js
@@ -4,7 +4,7 @@ import { clearNotices } from 'lib/actions/general';
 import { logOut } from 'lib/actions/auth';
 import Header from 'components/header';
 
-const Footer = () => <div className="footer">Made by <a href="http://foolord.com/">Payton</a>. Code on <a href="https://github.com/sirbrillig/voyageur-js-client">GitHub</a>. Logo by <a href="http://colemcdermott.co/">Cole</a>.</div>;
+const Footer = () => <div className="footer">Made by <a href="http://foolord.com/">Payton</a>. Code on <a href="https://github.com/sirbrillig/voyageur-js-client">GitHub</a>. Logo&nbsp;by&nbsp;<a href="http://colemcdermott.co/">Cole</a>.</div>;
 
 const LoggedIn = React.createClass( {
   propTypes: {
