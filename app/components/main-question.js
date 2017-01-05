c0ece038313f673e2082e8e0c7697b8b5d8b699c
Payton Swick <payton@foolord.com>
2017-01-04 20:40:23 -0500

    Adjust the MainQuestion text to use "Search for"

--- app/components/main-question.js
+++ app/components/main-question.js
@@ -3,12 +3,12 @@ import React from 'react';
 const MainQuestion = function( props ) {
   const getQuestionText = function( trip ) {
     if ( trip.length < 1 ) {
-      return 'Enter a location';
+      return 'Search for a location';
     }
     if ( trip.length === 1 ) {
-      return 'Enter another location and I\'ll tell you the distance of the trip';
+      return 'Search for another location and I\'ll tell you the distance of the trip';
     }
-    return 'Enter another location';
+    return 'Search for another location';
   };
   return <div className="question">{ getQuestionText( props.trip ) }</div>;
 };
