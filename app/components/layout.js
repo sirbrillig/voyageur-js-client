f1eebc805f345d66271c8786491bd74de9daca5e
Payton Swick <payton@foolord.com>
2017-01-09 18:14:21 -0500

    Clean up Layout

--- app/components/layout.js
+++ app/components/layout.js
@@ -4,42 +4,22 @@ import { clearNotices } from 'lib/actions/general';
 import { logOut } from 'lib/actions/auth';
 import Header from 'components/header';
 
-const Footer = () => <div className="footer">Made by <a href="http://foolord.com/">Payton</a>. Code on <a href="https://github.com/sirbrillig/voyageur-js-client">GitHub</a>. Logo&nbsp;by&nbsp;<a href="http://colemcdermott.co/">Cole</a>.</div>;
+const Footer = () => <div className="footer">Made&nbsp;by&nbsp;<a href="http://foolord.com/">Payton</a>. Code&nbsp;on&nbsp;<a href="https://github.com/sirbrillig/voyageur-js-client">GitHub</a>. Logo&nbsp;by&nbsp;<a href="http://colemcdermott.co/">Cole</a>.</div>;
 
-const Layout = React.createClass( {
-  propTypes: {
-    isAdmin: React.PropTypes.bool,
-    notices: React.PropTypes.object,
-    clearNotices: React.PropTypes.func.isRequired,
-    logOut: React.PropTypes.func.isRequired,
-  },
+const Layout = ( props ) => (
+  <div className="layout">
+    <Header errors={ props.notices.errors } onClearNotices={ props.clearNotices } onLogOut={ props.logOut } isAdmin={ props.isAdmin } />
+    { props.children }
+    <Footer />
+  </div>
+);
 
-  onClearNotices() {
-    this.props.clearNotices();
-  },
-
-  onLogOut() {
-    this.props.logOut();
-  },
-
-  renderMain() {
-    return (
-      <div className="layout">
-        { this.props.children }
-      </div>
-    );
-  },
-
-  render() {
-    return (
-      <div className="layout">
-        <Header errors={ this.props.notices.errors } onClearNotices={ this.onClearNotices } onLogOut={ this.onLogOut } isAdmin={ this.props.isAdmin } />
-          { this.renderMain() }
-        <Footer />
-      </div>
-    );
-  }
-} );
+Layout.propTypes = {
+  isAdmin: React.PropTypes.bool,
+  notices: React.PropTypes.object,
+  clearNotices: React.PropTypes.func.isRequired,
+  logOut: React.PropTypes.func.isRequired,
+};
 
 function mapStateToProps( state ) {
   const { auth, notices } = state;
