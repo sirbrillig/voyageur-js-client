0922a48514a777ce0305bde502765558cb621b94
Payton Swick <payton@foolord.com>
2017-01-30 14:16:44 -0500

    Remove createClass from LogInBox

--- app/components/log-in-box.js
+++ app/components/log-in-box.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-export default React.createClass( {
-  propTypes: {
-    showAuth: React.PropTypes.func.isRequired
-  },
-
-  render: function() {
-    return (
-      <div className="log-in-box">
-        <div className="log-in-box__content">
-          <img className="log-in-box__logo animated flipInX" src="/assets/logo-medium-smooth.png" />
-          <h1 className="log-in-box__title">Voyageur</h1>
-          <p className="log-in-box__subtitle">How far do you go?</p>
-          <a onClick={ this.props.showAuth } className="log-in-box__button btn btn-primary btn-lg">Let's find out!</a>
-        </div>
+const LogInBox = ( props ) => {
+  return (
+    <div className="log-in-box">
+      <div className="log-in-box__content">
+        <img className="log-in-box__logo animated flipInX" src="/assets/logo-medium-smooth.png" />
+        <h1 className="log-in-box__title">Voyageur</h1>
+        <p className="log-in-box__subtitle">How far do you go?</p>
+        <a onClick={ props.showAuth } className="log-in-box__button btn btn-primary btn-lg">Let's find out!</a>
       </div>
-    );
-  }
-} );
+    </div>
+  );
+};
+
+LogInBox.propTypes = {
+  showAuth: React.PropTypes.func.isRequired
+};
+
+export default LogInBox;
