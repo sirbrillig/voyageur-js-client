45f943d7a152c3d75f4ac72215845bc9a1345b74
Payton Swick <payton@foolord.com>
2017-01-30 14:06:26 -0500

    Remove createClass from TripLocation

--- app/components/trip-location.js
+++ app/components/trip-location.js
@@ -3,50 +3,54 @@ import classNames from 'classnames';
 import { DragSource, DropTarget } from 'react-dnd';
 import flow from 'lodash.flow';
 
-const TripLocation = React.createClass( {
-  propTypes: {
-    address: React.PropTypes.string.isRequired,
-    name: React.PropTypes.string,
-    isLoading: React.PropTypes.bool,
-    index: React.PropTypes.number.isRequired,
-    onRemoveTripLocation: React.PropTypes.func.isRequired,
-    connectDragSource: React.PropTypes.func.isRequired,
-    isDragging: React.PropTypes.bool.isRequired,
-    connectDropTarget: React.PropTypes.func.isRequired,
-    isOver: React.PropTypes.bool.isRequired,
-    onDrop: React.PropTypes.func.isRequired,
-  },
-
-  renderControls() {
-    if ( this.props.isLoading ) {
-      return <span className="trip-location__loading glyphicon glyphicon-refresh glyphicon-spin" />;
-    }
-    const onRemove = () => this.props.onRemoveTripLocation( this.props.index );
-    return (
-      <div className="btn-group btn-group-sm" role="group">
-        <button className="btn btn-default" onClick={ onRemove }>Remove</button>
-      </div>
-    );
-  },
-
-  render() {
-    const locationClassNames = classNames( 'trip-location row well well-sm', {
-      'trip-location--droppable': this.props.isOver,
-    } );
-    return this.props.connectDropTarget( this.props.connectDragSource(
-      <li className={ locationClassNames } >
-        <div className="trip-location__description col-xs-8" >
-          <h3 className="trip-location__description__name">{ this.props.name || this.props.address }</h3>
-        </div>
-        <div className="col-xs-4" >
-          <div className="trip-location__controls" >
-          { this.renderControls() }
-          </div>
-        </div>
-      </li>
-    ) );
+const TripLocationControls = ( props ) => {
+  if ( props.isLoading ) {
+    return <span className="trip-location__loading glyphicon glyphicon-refresh glyphicon-spin" />;
   }
-} );
+  const onRemove = () => props.onRemoveTripLocation( props.index );
+  return (
+    <div className="btn-group btn-group-sm" role="group">
+    <button className="btn btn-default" onClick={ onRemove }>Remove</button>
+    </div>
+  );
+};
+
+TripLocationControls.propTypes = {
+  isLoading: React.PropTypes.bool,
+  index: React.PropTypes.number.isRequired,
+  onRemoveTripLocation: React.PropTypes.func.isRequired,
+};
+
+const TripLocation = ( props ) => {
+  const locationClassNames = classNames( 'trip-location row well well-sm', {
+    'trip-location--droppable': this.props.isOver,
+  } );
+  return props.connectDropTarget( props.connectDragSource(
+    <li className={ locationClassNames } >
+      <div className="trip-location__description col-xs-8" >
+        <h3 className="trip-location__description__name">{ props.name || props.address }</h3>
+      </div>
+      <div className="col-xs-4" >
+        <div className="trip-location__controls" >
+          <TripLocationControls isLoading={ props.isLoading } index={ props.index } onRemoveTripLocation={ props.onRemoveTripLocation } />
+        </div>
+      </div>
+    </li>
+  ) );
+};
+
+TripLocation.propTypes = {
+  address: React.PropTypes.string.isRequired,
+  name: React.PropTypes.string,
+  isLoading: React.PropTypes.bool,
+  index: React.PropTypes.number.isRequired,
+  onRemoveTripLocation: React.PropTypes.func.isRequired,
+  connectDragSource: React.PropTypes.func.isRequired,
+  isDragging: React.PropTypes.bool.isRequired,
+  connectDropTarget: React.PropTypes.func.isRequired,
+  isOver: React.PropTypes.bool.isRequired,
+  onDrop: React.PropTypes.func.isRequired,
+};
 
 const dragSpec = {
   beginDrag( props ) {
