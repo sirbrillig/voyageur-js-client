c72700415496e6f586c4d7cb841a1bacc599fdc9
Payton Swick <payton@foolord.com>
2017-01-30 14:14:51 -0500

    Remove createClass from LoadingPanel

--- app/components/loading-panel.js
+++ app/components/loading-panel.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
-export default React.createClass( {
-  render() {
-    return (
-      <div key="loading-panel-panel" className="loading-panel">
-        <div className="loading-panel__content">
-          <img className="loading-panel__logo animated flipInX" src="/assets/logo-medium-smooth.png" />
-          <div className="loading-panel__text-area animated fadeInUp">
-            <h2 className="loading-panel__text animated pulse">Hang on just a moment, please!</h2>
-          </div>
+const LoadingPanel = () => {
+  return (
+    <div key="loading-panel-panel" className="loading-panel">
+      <div className="loading-panel__content">
+        <img className="loading-panel__logo animated flipInX" src="/assets/logo-medium-smooth.png" />
+        <div className="loading-panel__text-area animated fadeInUp">
+          <h2 className="loading-panel__text animated pulse">Hang on just a moment, please!</h2>
         </div>
       </div>
-    );
-  }
-} );
+    </div>
+  );
+};
 
+export default LoadingPanel;
