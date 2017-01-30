7fad865bf24b2afb9671f8c8e94c97cb8fa29728
Payton Swick <payton@foolord.com>
2017-01-30 14:21:50 -0500

    Remove createClass from ExportPanel and ImportPanel

--- app/components/import.js
+++ app/components/import.js
@@ -2,24 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { importLocations } from 'lib/actions/library';
 
-const ImportPanel = React.createClass( {
-  onImport() {
-    this.props.importLocations( this.textInput.value );
-  },
-
-  render() {
-    return (
-      <div className="import form-horizontal">
-        <div className="form-group">
-          <label htmlFor="import__text" className="import__text__label">Location Data (as JSON)</label>
-          <textarea className="import__text form-control" id="import__text" rows="10" ref={ input => this.textInput = input } />
-        </div>
-        <div className="form-group">
-          <button className="btn btn-lg btn-primary" onClick={ this.onImport }>Import Locations</button>
-        </div>
+const ImportPanel = ( props ) => {
+  const onImport = () => props.importLocations( this.textInput.value );
+  const saveRef = input => this.textInput = input;
+  return (
+    <div className="import form-horizontal">
+      <div className="form-group">
+        <label htmlFor="import__text" className="import__text__label">Location Data (as JSON)</label>
+        <textarea className="import__text form-control" id="import__text" rows="10" ref={ saveRef } />
       </div>
-    );
-  }
-} );
+      <div className="form-group">
+        <button className="btn btn-lg btn-primary" onClick={ onImport }>Import Locations</button>
+      </div>
+    </div>
+  );
+};
 
 export default connect( null, { importLocations } )( ImportPanel );
