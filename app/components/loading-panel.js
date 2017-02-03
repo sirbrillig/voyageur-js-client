26b38eeb6121b3cd646a57d3ae0add49af63eb6d
Payton Swick <payton@foolord.com>
2017-02-03 12:00:37 -0500

    Add alt to all img tags

--- app/components/loading-panel.js
+++ app/components/loading-panel.js
@@ -4,7 +4,7 @@ const LoadingPanel = () => {
   return (
     <div key="loading-panel-panel" className="loading-panel">
       <div className="loading-panel__content">
-        <img className="loading-panel__logo animated flipInX" src="/assets/logo-medium-smooth.png" />
+        <img className="loading-panel__logo animated flipInX" alt="Voyageur logo" src="/assets/logo-medium-smooth.png" />
         <div className="loading-panel__text-area animated fadeInUp">
           <h2 className="loading-panel__text animated pulse">Hang on just a moment, please!</h2>
         </div>
