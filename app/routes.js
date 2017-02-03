9493463612fae494ee5f1f644e379b876b8c5655
Payton Swick <payton@foolord.com>
2017-02-03 11:39:57 -0500

    Remove admin dashboard for now

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,6 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from 'lib/store';
 import App from 'components/app';
-import AdminDashboard from 'components/admin-dashboard';
 import ImportPanel from 'components/import';
 import ExportPanel from 'components/export';
 import LoggedIn from 'components/logged-in';
@@ -13,7 +12,6 @@ export default (
     <Router history={ browserHistory }>
       <Route path="/" component={ App }>
         <IndexRoute component={ LoggedIn } />
-        <Route path="/admin" component={ AdminDashboard } />
         <Route path="/import" component={ ImportPanel } />
         <Route path="/export" component={ ExportPanel } />
       </Route>
